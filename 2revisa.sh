b9c11923e291bc5225458efe1497d54941bfd77c
Martín Gutiérrez <martin.gutierrez@oracle.com>
2020-08-18 17:01:46 -0400

    Normal

--- 2revisa.sh
+++ 2revisa.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 export T=$1
-oci iam tenancy get --tenancy-id $OCI_TENANCY > tenancy.json
-export REPO=$(cat tenancy.json | jq '.data."home-region-key" | ascii_downcase').ocir.io
-export REPODOM=$(cat tenancy.json | jq '.data.name')
+#oci iam tenancy get --tenancy-id $OCI_TENANCY > tenancy.json
+oci iam compartment get --compartment-id $OCI_TENANCY > comproot.json
+oci iam region list > reg.json
+P=$(cat reg.json | jq '.data[] | select( .name == "'$OCI_REGION'" ) | .key | ascii_downcase')
+export REPO=$P.ocir.io
+export REPODOM=$(cat comproot.json | jq '.data.name')
 export userid=$(grep '"principal_id"' terraform.tfstate  | sort -u | awk -F'"' '{print $4}')
 export USU=$(oci iam user get --user-id $userid | jq -r '.data.name')
-#export TOKENCLA=$1
 if [ a$T == a ]; then
 	oci iam auth-token  create --description cloudShell --user-id $userid > token.json
 	export TOKENCLA=$(cat token.json | jq -r '.data.token')
@@ -18,5 +20,6 @@ $espera  nodes
 echo "######################################################################"
 docker login $REPO -u $REPODOM/$USU -p "$TOKENCLA"
 echo "######################################################################"
-oci ce node-pool get --profile $DOM --node-pool-id $(cat poolid.txt) > nodos.json
+oci ce node-pool get --node-pool-id $(cat poolid.txt) > nodos.json
+cat nodos.json | jq -r '.data.nodes[]."public-ip"' > ipnodo.txt
 cat nodos.json | jq '.data.nodes[].id' > nodo.txt
