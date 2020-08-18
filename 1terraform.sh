2a26c55f20e1dc9ab1e955c7c7e40bd8e1deeb8b
Martín Gutiérrez <martin.gutierrez@oracle.com>
2020-08-18 13:52:24 -0400

    Normal

--- 1terraform.sh
+++ 1terraform.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export TF_VAR_tenancy_ocid=$OCI_TENANCY
+export TF_VAR_region=$OCI_REGION
+ssh-keygen -f llave -N ""
+export TF_VAR_ssh_public_key=$(cat llave.pub)
 terraform init
 terraform plan
 terraform apply
